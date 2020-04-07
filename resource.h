7edfc6a83aa387acc95fd9f180a739ffbff22430
Kevin <anyboo@qq.com>
2020-04-07 20:39:59 +0800

    Update Version Number

--- resource.h
+++ resource.h
@@ -9,16 +9,16 @@
 
 #define MAIN				1 
 #define Major				1 
-#define Minor				9 
-#define FileIndex			7
+#define Minor				10
+#define FileIndex			9
 
 #define VTSFILEVERSION			MAIN,Major,Minor,FileIndex
 #define VTSFILEVERSION_STR		STRINGIZE(MAIN) "." STRINGIZE(Major) "." STRINGIZE(Minor) "." STRINGIZE(FileIndex)
 
 #define WMAIN				2 
 #define WMajor				0 
-#define WMinor				6 
-#define WFileIndex			79
+#define WMinor				8 
+#define WFileIndex			82
 
 #define VTSPRODUCTVERSION			WMAIN,WMajor,WMinor,WFileIndex
 #define VTSPRODUCTVERSION_STR		STRINGIZE(WMAIN) "." STRINGIZE(WMajor) "." STRINGIZE(WMinor) "." STRINGIZE(WFileIndex)
